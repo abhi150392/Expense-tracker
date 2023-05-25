7f7f2a5adf934c00489d0e360b5eccb92c2f0874
Abhishek Vijayakumar <Abhishekwhizzer@gmail.com>
2023-05-25 13:40:52 -0400

    added state updates

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-  /* const expenseDate = new Date(2023, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseItem = 184; */
+  // let title = props.title;
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    // title = "updated title";
+    setTitle("Updated title or title Changed!!!!");
+    console.log(title);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change Title!!!</button>
     </Card>
   );
 };
